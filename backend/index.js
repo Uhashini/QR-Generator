80ddd509562466958b76f0b90b42571de6adbeea
Uhashini <uhashininithilan@gmail.com>
2025-12-01 17:31:19 +0530

    Add Express backend QR generation API

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,39 @@
-import inquirer from "inquirer";
+import express from "express";
+import cors from "cors";
 import qr from "qr-image";
 import fs from "fs";
 
-inquirer
-  .prompt([
-    {
-      message: "Type in your URL: ",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-    fs.writeFile("URL.txt", url, (err) => {
-      if (err) throw err;
-      console.log("The file has been saved!");
-    });
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+app.post("/generate-qr", (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  // Create QR image and save to file (like your old script)
+  const qr_png = qr.image(url, { type: "png" });
+  const qrPath = "qr_img.png";
+  const writeStream = fs.createWriteStream(qrPath);
+  qr_png.pipe(writeStream);
+
+  // Save URL to text file
+  fs.writeFile("URL.txt", url, (err) => {
+    if (err) {
+      console.error(err);
     }
   });
+
+  // Also stream QR image back in the response
+  res.setHeader("Content-Type", "image/png");
+  const qrStream = qr.image(url, { type: "png" });
+  qrStream.pipe(res);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
